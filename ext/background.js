8605f39eed9809f03aea2067faf35f85146c9cdc
unknown <ZX@FZ-LINFEI-YE.fzzixun.com>
2022-05-06 22:08:30 +0800

    调用接口

---
+++ ext/background.js
@@ -0,0 +1,21 @@
+
+chrome.browserAction.onClicked.addListener(() => {
+    chrome.tabs.captureVisibleTab({ format: 'jpeg', quality: 100 }, (dataUrl) => {
+        if (!chrome.runtime.lastError) {
+            const imgData = dataUrl.split('base64,')[1];
+            fetch('https://www.paddlepaddle.org.cn/paddlehub-api/image_classification/chinese_ocr_db_crnn_mobile', {
+                method: "POST",
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'image': imgData
+                })
+            }).then(res => res.json()).then(data => {
+                console.log(data)
+             })
+        }
+    });
+});
+
