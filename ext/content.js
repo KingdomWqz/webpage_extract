f480669c2b0a651fcccc0458a34a653dfe6892ba
unknown <ZX@FZ-LINFEI-YE.fzzixun.com>
2022-05-10 21:22:47 +0800

    1688

--- ext/content.js
+++ ext/content.js
@@ -1,5 +1,9 @@
 let captureDiv = null;
 
+/**
+ * 截图
+ * @param {*} e 
+ */
 const capture_mousedown = (e) => {
     const div = document.createElement('DIV');
     div.style.position = 'absolute';
@@ -24,10 +28,117 @@ const capture_mouseup = (e) => {
     document.body.removeEventListener('mouseup', capture_mouseup);
 }
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === "capture") {
-            document.body.addEventListener('mousedown', capture_mousedown);
+const getPageContent = () => {
+    const reg = /class="(\S*)"/g;
+    const html = document.body.innerHTML;
+    const classes = html.match(reg);
+
+    // 统计class
+    let data = {};
+    for (let i = 0; i < classes.length; i++) {
+        const n = data[classes[i]] ? data[classes[i]] : 0;
+        data[classes[i]] = n + 1;
+    }
+    console.log(data);
+
+    // 统计个数，排除1个和2个，不大可能是搜索结果相关的class
+    let num = {};
+    for (const key in data) {
+        if (data[key] === 1 || data[key] === 2) continue;
+
+        const n = num[data[key]] ? num[data[key]] : 0;
+        num[data[key]] = n + 1;
+    }
+    console.log(num);
+
+    // 找最大
+    let max = null;
+    for (const key in num) {
+        if (max) {
+            if (num[key] > max.num) {
+                max = { id: key, num: num[key] }
+            }
+        } else {
+            max = { id: key, num: num[key] }
         }
+    }
+    console.log(max);
+
+    // 获取相关class
+    let result = [];
+    for (const key in data) {
+        if (data[key] == parseInt(max.id)) result.push(key.replace('class="', '').replace('"', ''));
+    }
+    console.log(result);
+
+    // 判断父级
+    let p = 0;
+    for (; p < result.length; p++) {
+        const allChilds = document.querySelector(`.${result[p]}`).getElementsByTagName('*');
+        let num = 0;
+        for (let j = 0; j < allChilds.length; j++) {
+            if (result.indexOf(allChilds[j].className) != -1) num++;
+        }
+        console.log(result[p], num);
+        if (num == result.length - 1) break;
+    }
+
+    // 提取示例
+    const extracts = [];
+    const allChilds = document.querySelector(`.${result[p]}`).getElementsByTagName('*');
+    console.log(allChilds);
+    for (let i = 0; i < allChilds.length; i++) {
+        if (allChilds[i].tagName == 'FONT') continue;
+        if (allChilds[i].firstChild && allChilds[i].firstChild.nodeType == 3) {
+            extracts.push(allChilds[i].innerText);
+        }
+    }
+    console.log(extracts);
+
+    chrome.runtime.sendMessage({
+        action: 'getHTML',
+        message: result,
+        parent: result[p],
+        extract: extracts
+    }, (response) => {
+        console.log(response);
+    });
+}
+
+const showElements = (data) => {
+    for (let i = 0; i < data.length; i++) {
+        const els = document.querySelectorAll(`.${data[i]}`);
+        for (let j = 0; j < 1; j++) {
+            console.log(els[j]);
+            els[j].style.border = 'solid 1px red';
+
+            // const rect = els[j].getBoundingClientRect();
+
+            // const div = document.createElement('DIV');
+            // div.style.position = 'absolute';
+            // div.style.border = 'solid 1px red';
+            // div.style.zIndex = '999';
+            // div.style.top = `${rect.top}px`;
+            // div.style.left = `${rect.left}px`;
+            // div.style.bottom = `${rect.bottom}px`;
+            // div.style.right = `${rect.right}px`;
+
+            // document.body.append(div);
+        }
+    }
+}
+
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+        // if (request.message === "capture") {
+        //     document.body.addEventListener('mousedown', capture_mousedown);
+        // }
+        if (request.action === 'getHTML') {
+            getPageContent();
+        }
+        if (request.action === 'showEls') {
+            showElements(request.data)
+        }
+        return true;
     }
 );
--- ext/popup.js
+++ ext/popup.js
@@ -1,12 +1,37 @@
 
-const capture = () => {
-    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "capture" });
-    });
-}
+let classData = [];
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('capture').addEventListener(
-        'click', capture);
+        'click', async () => {
+            const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+            const activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, { 'action': 'capture' });
+        });
+
+    document.getElementById('pageContent').addEventListener('click', async () => {
+        const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+        const activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { 'action': 'getHTML' });
+    })
+
+    // document.getElementById('show').addEventListener('click', async () => {
+    //     const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+    //     const activeTab = tabs[0];
+    //     chrome.tabs.sendMessage(activeTab.id, { 'action': 'showEls', 'data': classData });
+    // })
 });
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action == 'getHTML') {
+        classData = request.message;
+        document.querySelector('#classList').innerHTML = classData.join(', ');
+        document.querySelector('#parentClass').innerHTML = request.parent;
+        document.querySelector('#extract').innerHTML = request.extract.join(', ');
+    }
+    return true;
+})
+
+
+
+
