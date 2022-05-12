0684765b8e491b98988c1f100db75ac089fdfcfd
unknown <ZX@FZ-LINFEI-YE.fzzixun.com>
2022-05-12 21:50:10 +0800

    1688

--- ocr/ali.py
+++ ocr/ali.py
@@ -1,4 +1,14 @@
+from playwright.sync_api import sync_playwright
+
 from viapi.fileutils import FileUtils
+
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://www.1688.com')
+    
+
 file_utils = FileUtils('LTAI5tGcFYHWknLFRmjhfQgS', 'USJq3PIcX3QZIjVQseLPE2zYei7P4P')
 
 oss_url = file_utils.get_oss_url("demo.jpg", 'jpg', True)
